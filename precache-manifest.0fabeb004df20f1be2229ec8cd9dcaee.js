065abde6a2e3e0ad1c81cabc076d6c29b986274a
aditi-uma <f20180329@goa.bits-pilani.ac.in>
2020-07-24 00:52:27 +0530

    Updates

---
+++ precache-manifest.0fabeb004df20f1be2229ec8cd9dcaee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992ab77c18b9f4b2f86d297bf98047e7",
+    "url": "/Red-Rovers/index.html"
+  },
+  {
+    "revision": "f771c7d854aa9a840189",
+    "url": "/Red-Rovers/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "612c2b1f24f246305f9a",
+    "url": "/Red-Rovers/static/css/main.298c34d9.chunk.css"
+  },
+  {
+    "revision": "f771c7d854aa9a840189",
+    "url": "/Red-Rovers/static/js/2.a2f30b36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Red-Rovers/static/js/2.a2f30b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612c2b1f24f246305f9a",
+    "url": "/Red-Rovers/static/js/main.88d5529f.chunk.js"
+  },
+  {
+    "revision": "4233435b464171d94beb",
+    "url": "/Red-Rovers/static/js/runtime-main.97cbbabd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Red-Rovers/precache-manifest.91599e82c216ae9ace5a11167e1d68ab.js"
+  "/Red-Rovers/precache-manifest.0fabeb004df20f1be2229ec8cd9dcaee.js"
 );
 
 self.addEventListener('message', (event) => {
