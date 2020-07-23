a7407035632be88291d9a796ce2d3e214daa45d8
aditi-uma <f20180329@goa.bits-pilani.ac.in>
2020-07-23 14:55:41 +0530

    Updates

---
+++ precache-manifest.360ae9954009ba9935b36780c6893f62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e004521c6c7a4d541220106687a93c4",
+    "url": "/Red-Rovers/index.html"
+  },
+  {
+    "revision": "9840fa79ef22d9667165",
+    "url": "/Red-Rovers/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "720e36fd1ce23536062a",
+    "url": "/Red-Rovers/static/css/main.abb78e5c.chunk.css"
+  },
+  {
+    "revision": "9840fa79ef22d9667165",
+    "url": "/Red-Rovers/static/js/2.f08a6e8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Red-Rovers/static/js/2.f08a6e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720e36fd1ce23536062a",
+    "url": "/Red-Rovers/static/js/main.cb02adf2.chunk.js"
+  },
+  {
+    "revision": "4233435b464171d94beb",
+    "url": "/Red-Rovers/static/js/runtime-main.97cbbabd.js"
+  }
+]);
